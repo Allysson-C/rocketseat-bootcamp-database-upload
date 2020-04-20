5f3bc0988e39198ac8524219cc8a84375256a03e
Allysson C. da Silva <allysson.cheguera@hotmail.com>
2020-04-20 17:06:34 -0300

    :sparkles: challenge finalized

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,26 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne({ where: { id } });
+    const balance = await transactionsRepository.getBalance();
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exists');
+    }
+
+    if (balance.total - transaction.value < 0) {
+      throw new AppError(
+        'Operation cannot be completed, balance will be less than zero',
+      );
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
